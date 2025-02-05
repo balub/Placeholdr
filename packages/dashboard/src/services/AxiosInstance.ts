7aa9564fb94629ee09bab3d38e8805c15bd1ac0b
Balu Babu <balub997@gmail.com>
2025-02-06 02:20:31 +0530

    chore: added backend base url env

--- packages/dashboard/src/services/AxiosInstance.ts
+++ packages/dashboard/src/services/AxiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.VITE_HANOMI_BACKEND_URL,
+	baseURL: import.meta.env.VITE_PLACEHOLDR_BACKEND_URL,
 	withCredentials: true
 });
